6141fe36625a3cc7a3552282a9b2e75213357b33
Mark Lorenz <markjlorenz@dapplebeforedawn.com>
2014-07-05 17:30:05 -0400

    Enumerable#first_and_only!

--- lib/first_and_only.rb
+++ lib/first_and_only.rb
@@ -1,5 +1,12 @@
 require "first_and_only/version"
 
-module FirstAndOnly
-  # Your code goes here...
+module Enumerable
+  def first_and_only!
+    fail(FirstAndOnly::LengthNotOne, count) if first(2).count != 1
+    first
+  end
+
+  module FirstAndOnly
+    LengthNotOne = Class.new(StandardError)
+  end
 end
