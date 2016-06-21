7f8faa89a5a73816aec7a44068d3f5916d1ad7da
Derek Schneider <schneidermd1993@gmail.com>
2016-06-20 23:10:29 -0400

    Avoid calling count on Enumerable

--- lib/first_and_only.rb
+++ lib/first_and_only.rb
@@ -2,14 +2,14 @@ require "first_and_only/version"
 
 module Enumerable
   def first_and_only!
-    fail(FirstAndOnly::CountNotOne.new count) if first(2).count != 1
+    fail(FirstAndOnly::CountNotOne.new) if first(2).count != 1
     first
   end
 
   module FirstAndOnly
     class CountNotOne < StandardError
-      def initialize(count)
-        super("Expected the count to be 1, but it was #{count}.")
+      def initialize
+        super("Expected the count to be 1.")
       end
     end
   end
--- spec/first_and_only_spec.rb
+++ spec/first_and_only_spec.rb
@@ -10,18 +10,18 @@ describe "Enumerable#first_and_only!" do
     specify do
       expect { subject.first_and_only! }.to raise_error(
         Enumerable::FirstAndOnly::CountNotOne,
-        "Expected the count to be 1, but it was 0."
+        "Expected the count to be 1."
       )
     end
   end
 
   shared_examples_for "it_has_more_than_one_element" do
     specify do
-      fail "bad example" if subject.count < 2
+      fail "bad example" if subject.first(2).count < 2
 
       expect { subject.first_and_only! }.to raise_error(
         Enumerable::FirstAndOnly::CountNotOne,
-        "Expected the count to be 1, but it was #{subject.count}."
+        "Expected the count to be 1."
       )
     end
   end
@@ -71,6 +71,24 @@ describe "Enumerable#first_and_only!" do
 
     context "with more than on element" do
       subject { my_enumerable.new(:element, :element) }
+      it_behaves_like "it_has_more_than_one_element"
+    end
+
+  end
+
+  describe "infinite enumerable" do
+    let(:infinite_enumerable) do
+      Class.new do
+        include Enumerable
+
+        def each
+          yield(rand(1_000_000)) while true
+        end
+      end
+    end
+
+    context "with more than on element" do
+      subject { infinite_enumerable.new }
       it_behaves_like "it_has_more_than_one_element"
     end
 
